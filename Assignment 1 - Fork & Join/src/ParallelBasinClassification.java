fa921a4ce7c4b95e997b44a73101328013b75246
Chris Kay <kckateera@gmail.com>
2020-08-26 19:35:06 +0200

    Implement writing to file

--- Assignment 1 - Fork & Join/src/ParallelBasinClassification.java
+++ Assignment 1 - Fork & Join/src/ParallelBasinClassification.java
@@ -1,4 +1,5 @@
-import java.util.LinkedList;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 
@@ -7,7 +8,7 @@ public class ParallelBasinClassification extends RecursiveAction {
     int lo;
     int hi;
     Float[][] terrain2D;
-    static final int SEQUENTIAL_CUTOFF = 128;
+    static final int SEQUENTIAL_CUTOFF = 512;
     static long t0,t1; //Timing constants
 
     ParallelBasinClassification(Float[][] terrain2D, int lo, int hi) {
@@ -23,9 +24,9 @@ public class ParallelBasinClassification extends RecursiveAction {
         if (hi - lo < SEQUENTIAL_CUTOFF) {
             for(int i=lo; i < hi; i++) { //process rows
                 for (int j = 0; j < terrain2D[i].length; j++) {
-                    int[] coordinate = BasinClassification.check_neighbours(terrain2D, i, j);
-                    if (coordinate != null) {
-                       BasinClassification.parallelBasins.add(coordinate); //Append found basins to parallelBasins
+                    boolean coordinate = BasinClassification.check_neighbours(terrain2D, i, j);
+                    if (coordinate) {
+                       BasinClassification.parallelBasins[i][j] = true; //Append found basins to parallelBasins
                     }
                 }
             }
@@ -41,20 +42,37 @@ public class ParallelBasinClassification extends RecursiveAction {
 
     public static void main(String[] args) { //Entry point?
         Float[][] ogTerrain2D = BasinClassification.formulateArray(args);
-
         ForkJoinPool pool = new ForkJoinPool();
         ParallelBasinClassification task = new ParallelBasinClassification(ogTerrain2D, 0, (ogTerrain2D.length-1));
+
         System.gc();
         t0=System.currentTimeMillis();
         pool.invoke(task);
         t1=System.currentTimeMillis();
+
         //ForkJoinPool.commonPool().invoke(task);
-        System.out.println(BasinClassification.parallelBasins.size());
-        for(int[] basin: BasinClassification.parallelBasins ){
-            System.out.print(basin[0]);
-            System.out.println(basin[1]);
-        }
+        System.out.println(BasinClassification.noBasins);
         System.out.println(t1-t0);
+        //Write to output file
+        try {
+            FileWriter myWriter = new FileWriter(args[1]);//use args[1]
+
+            System.out.println(BasinClassification.noBasins);
+            myWriter.write(BasinClassification.noBasins+"\n");
+
+            for (int i = 0; i < BasinClassification.sequentialBasins.length; i++) {
+                for (int j = 0; j < BasinClassification.parallelBasins[i].length; j++) {
+                    if(BasinClassification.parallelBasins[i][j]) {
+                        System.out.println(i+" "+j);
+                        myWriter.write(i+" "+j+"\n");
+                    }
+                }
+            }
+            myWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
